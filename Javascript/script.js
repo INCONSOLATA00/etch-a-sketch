05417adbf0ec8a321bc890201255ed1cdb5ef666
INCONSOLATA00 <gabe.rattigan@gmail.com>
2023-11-26 00:13:43 -0500

    added logic
    - created enclosed, wrapped grid

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,9 +1,19 @@
 const wrapper = document.querySelector('.wrapper');
-const square = document.createElement('div');
-square.className = "square";
 
 (() => {
-for(i = 0; i < 16; i++) {
-wrapper.append(square);
+for(i = 0; i < 16 * 16; i++) 
+{
+const square = document.createElement('div');
+square.style.cssText = 
+`
+display: flex;
+align-content: flex-start; 
+
+height: 37.5px;
+width: 37.5px;
+background-color: lightgrey;
+`
+wrapper.appendChild(square);
 }
+
 })();
