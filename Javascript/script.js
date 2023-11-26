023e99d8c0356a1f9d6a8a745a5d070bc6cfe66e
INCONSOLATA00 <gabe.rattigan@gmail.com>
2023-11-25 21:34:51 -0500

    pseudo / rough

---
+++ Javascript/script.js
@@ -0,0 +1,9 @@
+const wrapper = document.querySelector('.wrapper');
+const square = document.createElement('div');
+square.className = "square";
+
+(() => {
+for(i = 0; i < 16; i++) {
+wrapper.append
+}
+})();
