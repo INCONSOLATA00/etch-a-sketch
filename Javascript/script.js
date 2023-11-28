7e5f3268f3a40f4d7cb862b567f699efedcd2714
INCONSOLATA00 <gabe.rattigan@gmail.com>
2023-11-28 09:26:45 -0500

    indices now listen for mouseover

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,9 +1,10 @@
 const wrapper = document.querySelector('.wrapper');
 
 (() => {
-for(i = 0; i < 16 * 16; i++) 
+for(let i = 0; i < 16 * 16; i++) 
 {
-const square = document.createElement('div');
+const square = document.createElement('div' + i);
+
 square.style.cssText = 
 `
 display: flex;
@@ -14,6 +15,14 @@ width: 37.5px;
 background-color: lightgrey;
 `
 wrapper.appendChild(square);
+
+square.addEventListener('mouseover', alsoSquare)
+function alsoSquare(e) {
+console.log(`runs ${e.target}`);   
+}
 }
 
+
 })();
+
+
