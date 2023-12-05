00687232771b4523ae53e2fb155c603042df3e5a
INCONSOLATA00 <gabe.rattigan@gmail.com>
2023-12-04 21:16:26 -0500

    progress staged

--- Javascript/script.js
+++ Javascript/script.js
@@ -2,35 +2,39 @@ const wrapper = document.querySelector('.wrapper');
 const button = document.querySelectorAll('button');
 
 (() => {
-for(let i = 0; i < 16 * 16; i++) 
-{
-const square = document.createElement('div' + i);
+    for (let i = 0; i < 16 * 16; i++) {
+        const square = document.createElement('div');
 
-square.style.cssText = 
+        square.id = 'div' + i;
+        square.style.cssText =
 `
 display: flex;
-align-content: flex-start; 
-
 height: 37.5px;
 width: 37.5px;
 background-color: lightgrey;
 `
-wrapper.appendChild(square);
 
-square.addEventListener('mouseover', alsoSquare);
-function alsoSquare(e) {
-console.log(`runs ${e.target}`);   
-}
-square.addEventListener('mousedown', squareEngaged);
-squareEngaged = setInterval(function() {
-console.log('indice held');
-}, 50);
+const squareEngaged = () => {
+    setInterval(function () { // initialize; - *IS RUN* 2ND
+        console.log('indice held');
+    }, 50);
 }
 
+if (square.onmousedown == true) {
+    console.log('reached');
+    square.addEventListener('mouseup', clearInterval(squareEngaged)); // listen for onmouseup after onmousedown
+}
 
+function clearInterval() { // *IS NOT RUN*
+    clearInterval(squareEngaged);
+}
 
+square.addEventListener('mousedown', squareEngaged); // *IS RUN* 1ST
+
+        wrapper.appendChild(square);
+        square.addEventListener('mouseover', alsoSquare);
+        function alsoSquare(e) {
+        console.log(`runs ${e.target.id}`);
+        }
+    }
 })();
-
-
-
-
