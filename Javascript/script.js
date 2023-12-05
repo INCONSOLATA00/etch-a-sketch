70176e347e28de90b5962d9c94c4aa3d609329c4
INCONSOLATA00 <gabe.rattigan@gmail.com>
2023-12-05 11:12:49 -0500

    progress staged
    discerning, method to allocate new indice -

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,39 +1,51 @@
 const wrapper = document.querySelector('.wrapper');
 const button = document.querySelectorAll('button');
+let array = [];
 
-(() => {
-    for (let i = 0; i < 16 * 16; i++) {
-        const square = document.createElement('div');
 
-        square.id = 'div' + i;
-        square.style.cssText =
-`
+for (let i = 0; i < 16 * 16; i++) {
+    const square = document.createElement('div');
+
+    square.id = 'div' + i;
+    square.style.cssText =
+        `
 display: flex;
 height: 37.5px;
 width: 37.5px;
 background-color: lightgrey;
 `
-let theInterval;
+    array.push(square);
+    wrapper.appendChild(square);
 
-function squareEngaged() {
-// if(theInterval) { clearInterval(theInterval);}
-theInterval = setInterval(() => { console.log('mouse held');
-},350); 
+    square.addEventListener('mouseover', alsoSquare);
+
+    function alsoSquare(e) {
+    console.log(`runs ${e.target.id}`);
+    }
 }
+
+let theInterval;
+let clickEvent = new Event('click');
+
+function squareEngaged(e) {
+
+    let targetId = e.target.id;
+    if (targetId.startsWith('div')) {
+
+    theInterval = setInterval(() => {
+        console.log('mouse held');
+        document.getElementById(e.target.id).dispatchEvent(clickEvent); // WORKS
+        document.getElementById(e.target.id).style.backgroundColor = 'red';
+    }, 300);
+}}
 
 function squareDisengaged() {
-console.log('(!)mouse held');
-clearInterval(theInterval);    
+    console.log('(!)mouse held');
+    clearInterval(theInterval);
 }
-if (document.onmousedown == false) { // *prevent instance overlap*
-document.addEventListener('mousedown', squareEngaged);
-}
-document.addEventListener('mouseup', squareDisengaged);
 
-        wrapper.appendChild(square);
-        square.addEventListener('mouseover', alsoSquare);
-        function alsoSquare(e) {
-        console.log(`runs ${e.target.id}`);
-        }
-    }
-})();
+
+window.addEventListener('mousedown', squareEngaged);
+window.addEventListener('mouseup', squareDisengaged);
+
+//Pseudo: on mousedown if(div.target.id ==...
