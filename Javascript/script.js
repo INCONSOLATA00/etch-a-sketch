0db75853ac34300b64df8d0f3151215198745649
INCONSOLATA00 <gabe.rattigan@gmail.com>
2023-12-12 22:19:34 -0500

    progress staged

--- Javascript/script.js
+++ Javascript/script.js
@@ -19,24 +19,28 @@ width: ${sqrSize / 2}px;
 background-color: lightgrey;
 `
 
-    wrapper.appendChild(square); // despite appending square; actually appending it's new designation of square.id
+    wrapper.appendChild(square); // is new designation
 
-    square.addEventListener('dragstart',(e)=>{
+    function l1(e) {
         e.preventDefault();
-      })
-
-    square.addEventListener('mousedown', function(e) {
+    }
+    square.addEventListener('dragstart', l1);
+    
+    function l2(e) {
         document.getElementById(e.target.id).style.backgroundColor = 'black';
-    })
-
-    square.addEventListener('mouseover', function(e) {
+    }
+    square.addEventListener('mousedown', l2);
+    
+    function l3(e) {
         if(updateStatus == true) {
             document.getElementById(e.target.id).style.backgroundColor = 'black';
-
         }
-    })
+    }
+    square.addEventListener('mouseover', l3);
 
 }
+
+// ------------------------------------------------------------------
 
 window.addEventListener('mousedown', function() {
 updateStatus = true;
@@ -44,6 +48,8 @@ updateStatus = true;
 window.addEventListener('mouseup', function() {
 updateStatus = false;
 });
+
+// ------------------------------------------------------------------
 
 var slider = document.querySelector("#alsoRange");
 slider.oninput = function() {
@@ -54,6 +60,12 @@ let squareId = document.getElementById('div' + i);
 if(squareId) {
 
 // remove event listener(s)
+
+square.removeEventListener('dragstart', l1);
+
+square.removeEventListener('mousedown', l2);
+
+square.removeEventListener('mouseover', l3);
 
 square.parentNode.removeChild(squareId); 
 }}
@@ -70,9 +82,13 @@ width: ${1200 / slider.value / 2}px;
 background-color: lightgrey;
 `
 
-
-
 wrapper.appendChild(square);
+
+square.addEventListener('dragstart', l1);
+
+square.addEventListener('mousedown', l2);
+
+square.addEventListener('mouseover', l3);
 
 }}
 
