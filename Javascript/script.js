26725352734009145cf8831b2f09a0fbd223edc0
INCONSOLATA00 <gabe.rattigan@gmail.com>
2023-12-13 23:27:53 -0500

    *

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,15 +1,26 @@
 const wrapper = document.querySelector('.primary-wrapper');
+let slider = document.querySelector("#alsoRange");
 let updateStatus;
 
-let sqrMult = 10;
+let sqrMult = 32;
 let sqrAmt = sqrMult * sqrMult;
 let sqrSize = 1200 / sqrMult; 
 
 let square ;
+let elements;
+
+function yeet(){
+while(elements.length > 0){
+square.parentNode.removeChild(elements[0]);
+}
+}
 
 for (let i = 0; i < sqrAmt; i++) {
-    square = document.createElement('div');
 
+    square = document.createElement('div');
+    square.className = 'dynamicDiv'; // initializer ... should technically work on later instances
+
+    elements = document.getElementsByClassName('dynamicDiv');
     square.id = 'div' + i;
     square.style.cssText =
         `
@@ -18,12 +29,11 @@ height: ${sqrSize / 2}px;
 width: ${sqrSize / 2}px;
 background-color: lightgrey;
 `
-
-    wrapper.appendChild(square); // is new designation
-
+    wrapper.appendChild(square); 
+    
     function l1(e) {
         e.preventDefault();
-    }
+}
     square.addEventListener('dragstart', l1);
     
     function l2(e) {
@@ -37,10 +47,8 @@ background-color: lightgrey;
         }
     }
     square.addEventListener('mouseover', l3);
-
 }
 
-// ------------------------------------------------------------------
 
 window.addEventListener('mousedown', function() {
 updateStatus = true;
@@ -49,30 +57,25 @@ window.addEventListener('mouseup', function() {
 updateStatus = false;
 });
 
-// ------------------------------------------------------------------
-
-var slider = document.querySelector("#alsoRange");
 slider.oninput = function() {
 
-for (let i = 0; i < sqrAmt; i++) {
+for (let i = 0; i < slider.value * slider.value; i++) {
 
 let squareId = document.getElementById('div' + i);
-if(squareId) {
-
-// remove event listener(s)
+if(squareId) { // save for later functionality*
 
 square.removeEventListener('dragstart', l1);
-
 square.removeEventListener('mousedown', l2);
-
 square.removeEventListener('mouseover', l3);
-
-square.parentNode.removeChild(squareId); 
+yeet();
 }}
 
 for (let i = 0; i < slider.value * slider.value; i++) {
-square = document.createElement('div');
 
+square = document.createElement('div');
+square.className = 'dynamicDiv'; 
+
+elements = document.getElementsByClassName('dynamicDiv');
 square.id = 'div' + i;
 square.style.cssText =
         `
@@ -83,12 +86,11 @@ background-color: lightgrey;
 `
 
 wrapper.appendChild(square);
-
 square.addEventListener('dragstart', l1);
-
 square.addEventListener('mousedown', l2);
-
 square.addEventListener('mouseover', l3);
 
-}}
 
+console.log(` slider pos ${slider.value}`);
+}}
+console.log(` slider true ${slider.value * slider.value}`);
