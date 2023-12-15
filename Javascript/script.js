faee1960d8f39acc3a6c15d68b775e7514c15c3f
INCONSOLATA00 <gabe.rattigan@gmail.com>
2023-12-15 02:15:59 -0500

    add functional toggle button

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,6 +1,8 @@
 const wrapper = document.querySelector('.primary-wrapper');
 let slider = document.querySelector("#alsoRange");
+
 let updateStatus;
+let toggleProgressive = true;
 
 let sqrMult = 32;
 let sqrAmt = sqrMult * sqrMult;
@@ -8,6 +10,16 @@ let sqrSize = 1200 / sqrMult;
 
 let square ;
 let elements;
+
+function progressive(){ //  see rgba values*
+if(toggleProgressive == true) {
+toggleProgressive = false;
+console.log(toggleProgressive);
+
+} else if(toggleProgressive == false) { // upate status is responsible for the mouse down / mouseup events, nothing else*
+toggleProgressive = true;
+console.log(toggleProgressive);
+}}
 
 function yeet(){
 while(elements.length > 0){
@@ -23,7 +35,7 @@ for (let i = 0; i < sqrAmt; i++) {
     square = document.createElement('div');
     square.className = 'dynamicDiv';
     elements = document.getElementsByClassName('dynamicDiv');
-    square.id = 'div' + i; // is static
+    square.id = 'div' + i;
     square.style.cssText =
         `
 display: flex;
@@ -46,6 +58,8 @@ background-color: lightgrey;
     function l3(e) {
         if(updateStatus == true) {
             document.getElementById(e.target.id).style.backgroundColor = 'black';
+        } else if(updateStatus == true && toggleProgressive == true) { // WAS
+            document.getElementById(e.target.id).style.backgroundColor = 'red'; // += see shade
         }
     }
     square.addEventListener('mouseover', l3);
@@ -64,7 +78,7 @@ slider.oninput = function() {
 for (let i = 0; i < slider.value * slider.value; i++) {
 
 let squareId = document.getElementById('div' + i);
-if(squareId) { // save for later functionality* - check integrity
+if(squareId) { // do unique function for individual id's here
 yeet();
 }}
 
@@ -88,7 +102,6 @@ square.addEventListener('dragstart', l1);
 square.addEventListener('mousedown', l2);
 square.addEventListener('mouseover', l3);
 
-
 console.log(` slider pos ${slider.value}`);
 }}
-console.log(` slider init ${slider.value * slider.value}`);
+
